11325fb43cfbd7da58d1f3669f65b222f42f29a9
kkfnui <kkfnui@126.com>
2012-01-11 10:17:13 +0800

    Commit codes that has been finished last year.

---
+++ Script/sidemenu.js
@@ -0,0 +1,27 @@
+﻿//左边菜单
+
+$(function () {
+
+    //控制菜单的显示和隐藏
+    $(".expand_container").bind("click", function () {
+        $(this).parent().parent().find("li").toggle("500");
+        $(this).find(".icon").toggleClass("cssicon").toggleClass("icon_shrink");
+    });
+    //隐藏或显示左菜单面板
+    var hasleft = true;
+    $("#resize").bind("click", function () {
+        if (hasleft == true) {
+            $("#left").hide();
+            $("#right").css("margin-left", 5);
+            hasleft = false;
+            $("#control_icon").attr("src", "../Style/image/expand.gif");
+        } else {
+            $("#right").css("margin-left", 205);
+            $("#left").show();
+            hasleft = true;
+            $("#control_icon").attr("src", "../Style/image/reduce.gif");
+        }
+
+    });
+
+})
